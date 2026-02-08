be29e37a038e00b7a87578d0d6951e8476071235
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-02-08 11:35:38 -0500

    fix configure

--- StaticHTTPPackets.cpp
+++ StaticHTTPPackets.cpp
@@ -3,6 +3,8 @@
 #include <filesystem>
 #include <fstream>
 #include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+#include "StaticResponseProcessorHTTP.h"
 
 namespace fs = std::filesystem;
 
@@ -10,14 +12,14 @@ namespace fs = std::filesystem;
     push) // disable msvc's complaining about us not saving the processors in
           // vars, they'll be cleaned up when our program ends.
 #pragma warning(disable : 4101)
-void RegisterStaticHTTPHandlerFromFile(std::string route, std::string filename)
+static void RegisterStaticHTTPHandlerFromFile(std::string route, std::string filename)
 {
     std::ifstream fileres(filename);
     if (!fileres.is_open())
     {
         throw std::runtime_error("failed to open file");
     }
-    json res = json::parse(fileres);
+    json const res = json::parse(fileres);
     fileres.close();
     spdlog::info("registered static HTTP route {} from json file at {}", route,
                  filename);
@@ -26,40 +28,42 @@ void RegisterStaticHTTPHandlerFromFile(std::string route, std::string filename)
 }
 #pragma warning(pop)
 
-void RegisterStaticHTTPHandlers()
+static void RegisterStaticHTTPHandlers()
 {
     for (const auto &file : fs::recursive_directory_iterator(
              ResourcesUtilities::GetResourcesFolder() / "payloads" / "static" /
              "game"))
     {
-        if (!fs::is_regular_file(file))
+        if (!fs::is_regular_file(file)) {
             continue;
+}
         std::string route =
             (fs::absolute(file.path().parent_path()) / file.path().stem())
                 .string();
-        std::string prefixString =
+        std::string const prefixString =
             fs::absolute(ResourcesUtilities::GetResourcesFolder() / "payloads" /
                          "static" / "game")
                 .string();
         route.erase(route.find(prefixString), prefixString.size());
-        std::replace(route.begin(), route.end(), '\\', '/');
+        std::ranges::replace(route, '\\', '/');
         RegisterStaticHTTPHandlerFromFile(route, file.path().string());
     }
     for (const auto &file : fs::recursive_directory_iterator(
              ResourcesUtilities::GetResourcesFolder() / "payloads" / "static" /
              "social"))
     {
-        if (!fs::is_regular_file(file))
+        if (!fs::is_regular_file(file)) {
             continue;
+}
         std::string route =
             (fs::absolute(file.path().parent_path()) / file.path().stem())
                 .string();
-        std::string prefixString =
+        std::string const prefixString =
             fs::absolute(ResourcesUtilities::GetResourcesFolder() / "payloads" /
                          "static" / "social")
                 .string();
         route.erase(route.find(prefixString), prefixString.size());
-        std::replace(route.begin(), route.end(), '\\', '/');
+        std::ranges::replace(route, '\\', '/');
         RegisterStaticHTTPHandlerFromFile(route, file.path().string());
     }
 }
