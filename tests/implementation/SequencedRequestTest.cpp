910eaaf3dcf15ff2b238addaeff015877e3ca86a
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-02-11 16:20:08 -0500

    interim

--- tests/implementation/SequencedRequestTest.cpp
+++ tests/implementation/SequencedRequestTest.cpp
@@ -3,15 +3,32 @@
 #include <HTTPRequestTest.h>
 #include <WebsocketRequestTest.h>
 #include <fstream>
-#include <stack>
+#include <vector>
 
-void ApplyTestRequestInserts(json& payload, const std::stack<json>& responses) {
-
+static void ApplyTestRequestInserts(json& payload, const std::vector<json>& responses) {
+    for (std::pair<std::string, json> kv: payload) {
+        if (!kv.second.is_string()) {
+            continue;
+        }
+        std::string value = kv.second.get<std::string>();
+        size_t endPos = value.find('}');
+        if (!value.starts_with("${") || endPos == std::string::npos) {
+            continue;
+        }
+        std::string toInterpret = value.substr(3, value.size() - 2 - endPos);
+        if (toInterpret.starts_with("responses")) {
+            size_t endBrace = toInterpret.find(']');
+            int resNumber = stoi(toInterpret.substr(11, toInterpret.size() - endBrace));
+            std::string jsonPath = toInterpret.substr(endBrace + 1, toInterpret.size());
+            std::vector<std::string> pathSplit; // split at every period
+            responses.at(resNumber)
+        }
+    }
 }
 
 TEST_P(SequencedRequestTest, ResponseValidation) {
     std::queue<fs::path> requests;
-    std::stack<json> responses;
+    std::vector<json> responses;
     int i = 0;
     while (true) {
         fs::path curTestPath = GetParam() / (std::to_string(i) + ".json");
@@ -38,12 +55,12 @@ TEST_P(SequencedRequestTest, ResponseValidation) {
             ApplyTestRequestInserts(testJson["requestBody"], responses);
             json out;
             RunWebsocketTest(testJson, out);
-            responses.push(out["response"]["payload"]);
+            responses.push_back(out["response"]["payload"]);
         } else {
             // http request
             json out;
             RunHTTPTest(testJson, out);
-            responses.push(out);
+            responses.push_back(out);
         }
     }
 }
